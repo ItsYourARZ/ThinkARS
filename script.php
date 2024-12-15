47a51da5b31354de0336298e3e9c27586c85bc1b
ItsYourARZ <anirraj2022@gmail.com>
2024-12-15 17:23:14 +0530

    update

--- script.php
+++ script.php
@@ -5,7 +5,11 @@ $recaptcha_response = $_POST['g-recaptcha-response'];
 // Verify the reCAPTCHA response using Google's API
 $url = 'https://www.google.com/recaptcha/api/siteverify';
 $params = array(
+<<<<<<< Updated upstream
     'secret' => '6LfDZHsqAAAAACLAlt_aiaxL3vN0TFGyVkW5VEV-',
+=======
+    'secret' => 'YOUR_SECRET_KEY',
+>>>>>>> Stashed changes
     'response' => $recaptcha_response
 );
 $url = $url.'?'.http_build_query($params);
