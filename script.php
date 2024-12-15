4fdd63ad76aed17742dcb6cf6c179818e7e7e6b0
ItsYourARZ <anirraj2022@gmail.com>
2024-12-15 17:21:28 +0530

    Create script.php

---
+++ script.php
@@ -0,0 +1,27 @@
+<?php
+// Get the reCAPTCHA response from the form
+$recaptcha_response = $_POST['g-recaptcha-response'];
+
+// Verify the reCAPTCHA response using Google's API
+$url = 'https://www.google.com/recaptcha/api/siteverify';
+$params = array(
+    'secret' => '6LfDZHsqAAAAACLAlt_aiaxL3vN0TFGyVkW5VEV-',
+    'response' => $recaptcha_response
+);
+$url = $url.'?'.http_build_query($params);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+$resultJson = json_decode($result, true);
+
+if ($resultJson['success']) {
+    // reCAPTCHA is valid, process the form data
+    // ...
+} else {
+    // reCAPTCHA is invalid, handle the error
+    // ...
+}
