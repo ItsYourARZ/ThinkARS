64547b7a0cc264c9a33f9620046cdf0614c9a02e
Anirudh Rajesh <anirraj2022@gmail.com>
2025-03-28 16:52:06 +0530

    Update fetch_sitekey.js

--- components/scripts/fetch_sitekey.js
+++ components/scripts/fetch_sitekey.js
@@ -1,4 +1,4 @@
-fetch("/.netlify/functions/recaptcha-verify")
+fetch("https://thinkars.netlify.app/.netlify/functions/recaptcha-verify")
     .then(response => response.json())
     .then(data => {
       document.querySelector(".g-recaptcha").setAttribute("data-sitekey", data.siteKey);
