a508b4ffd05d2f72070cdab08c7a3a5656a37d18
Anirudh Rajesh <anirraj2022@gmail.com>
2025-03-28 17:25:48 +0530

    Change

--- components/scripts/fetch_sitekey.js
+++ components/scripts/fetch_sitekey.js
@@ -1,6 +1,5 @@
 fetch("/.netlify/functions/verify-recaptcha")
 .then(response => response.json())
 .then(data => {
-    document.getElementById("recaptcha-container").innerHTML = 
-        `<div class="g-recaptcha" data-sitekey="${data.siteKey}" data-callback="onRecaptchaSuccess"></div>`;
+    document.getElementById("recaptcha-container").innerHTML = data.siteKey;
 });
