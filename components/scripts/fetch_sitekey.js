bad1b43127165e844a1d0e22544fcc85bdf9cd62
Anirudh Rajesh <anirraj2022@gmail.com>
2025-03-28 17:28:27 +0530

    Update fetch_sitekey.js

--- components/scripts/fetch_sitekey.js
+++ components/scripts/fetch_sitekey.js
@@ -1,4 +1,4 @@
-fetch("/.netlify/functions/verify-recaptcha")
+fetch("/.netlify/functions/recaptcha-verify")
 .then(response => response.json())
 .then(data => {
     document.getElementById("recaptcha-container").innerHTML = data.siteKey;
