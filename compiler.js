0aa162e522e9f5ea4a67fe9f3b01c9e8218b69dd
Anirudh Rajesh <anirraj2022@gmail.com>
2025-08-14 21:35:43 +0530

    Update compiler.js

--- compiler.js
+++ compiler.js
@@ -10,7 +10,7 @@ let suggestion = "";
 
 async function fetchSuggestion(code) {
     try {
-        const res = await fetch("/suggest", {
+        const res = await fetch(".netlify/functions/suggest", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ prompt: code })
