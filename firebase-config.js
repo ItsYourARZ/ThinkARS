39c52dfacb94e5c7a87e61bb58c99403400125ca
Anirudh Rajesh <anirraj2022@gmail.com>
2025-10-26 21:50:57 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-apiKey: process.env.FIREBASE_COUNTER_API,
-authDomain: "the-ars-team---counter.firebaseapp.com",
-projectId: "the-ars-team---counter",
-storageBucket: "the-ars-team---counter.firebasestorage.app",
-messagingSenderId: process.env.FIREBASE_COUNTER_SENDER_ID,
-appId: "1:193540636912:web:266facaa7bcabfbf1d4235",
-measurementId: "G-N50YTEL8JB"
+apiKey: import.meta.env.FIREBASE_COUNTER_API,
+authDomain: import.meta.env.FIREBADE_COUNTER_AUTHDOMAIN,
+projectId: import.meta.env.FIREBADE_COUNTER_PROJECTID,
+storageBucket: import.meta.env.FIREBASE_COUNTER_STORAGEBUCKET,
+messagingSenderId: import.meta.env.FIREBASE_COUNTER_SENDER_ID,
+appId: import.meta.env.FIREBASE_COUNTER_APP_ID,
+measurementId: import.meta.env.FIREBASE_COUNTER_MEASUREMENTID,
 };
