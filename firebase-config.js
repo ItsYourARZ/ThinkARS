8fbc90cb0207732cca23b0d7d97767bf049a9959
Anirudh Rajesh <anirraj2022@gmail.com>
2025-10-26 22:08:47 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,3 @@
-// firebase-config.js
-import { initializeApp } from "./firebase/app";
-import { getFirestore } from "./firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
